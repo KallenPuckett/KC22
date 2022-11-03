81ed1606f8b8e29434b3bfb83d1e221ba1787316
KallenPuckett <105237240+KallenPuckett@users.noreply.github.com>
2022-11-02 22:05:50 -0400

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,17 @@
+﻿namespace Knowledgecheck2
+{
+    internal class Dog
+    {
+        public bool IsPug { get; set; }
+
+        public Dog(bool isPug, string name) : base(name)
+        {
+            IsPug = isPug;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $": is {(IsPug ? "" : "not")} a Pug";
+        }
+    }
+}
